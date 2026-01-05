b5c43b73fb3c9d2c0d88ab85fa8cf4a940f76361
RohitS0612 <shetakeyash@gmail.com>
2026-01-05 20:27:41 +0530

    comtent chnge and modification

--- src/windows/Contact.jsx
+++ src/windows/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
 
 
     <div className="p-5 space-y-5">
-        <img src="/images/adrian.jpg" alt="Adrian" className="w-20 rounded-full" />
+        <img src="/images/profile.jpg" alt="Adrian" className="w-20 h-20 rounded-full" />
         <h3>Let's Connect </h3>
         <p>Got an idea? A bug to squash? Or just wanna talk tech I'm in.</p>
 
